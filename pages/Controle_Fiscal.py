245815057de6c70b91c274a150a04c006bd33711
luannevesjp-ops <luannevesjp@outlook.com>
2026-06-01 01:00:04 -0300

    Atualização 31/05-21

--- pages/Controle_Fiscal.py
+++ pages/Controle_Fiscal.py
@@ -26,7 +26,7 @@ _ABA = {
     "controle_simples":   "SIMPLES NACIONAL",
     "parcelamentos":      "PARCELAMENTOS",
     "senhas_acessos":     "SENHAS E ACESSOS",
-    "alteracao_empresa":  "ALTERACAO DE EMPRESA",
+    "alteracao_empresa":  "ALTERACOES",
     "obrigacoes_prazos":  "OBRIGAÇÕES E PRAZOS",
 }
 
